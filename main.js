f2a4c7333268b2ad53dc524c2f53f6c292f842cf
Rashee91010 <78844718+Rashee91010@users.noreply.github.com>
2021-11-10 16:36:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+mustachex=0;
+mustachey=0;
+
+function preload(){
+    mustache=loadImage('https://i.postimg.cc/wvsnvxsr/moustache-removebg-preview.png');
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelloaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelloaded(){
+    console.log("poseNet is initialized");
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+        {
+            console.log(results);
+            mustachex=results[0].pose.nose.x-15;
+            mustachey=results[0].pose.nose.y;
+        }
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    image(mustache,mustachex,mustachey,30,30);
+}
+
+function take_snapshot(){
+    save('myfilterimage2.png');
+    console.log("HI");
+}
